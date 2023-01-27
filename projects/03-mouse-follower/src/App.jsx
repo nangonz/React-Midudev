e13abb4729e2a85962ded202ff328ae119be7de4
nangonz <nangonz91@gmail.com>
2023-01-27 14:01:20 -0300

    mouse pointer follower

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react'
 
 function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+      console.log('handleMove', { clientX, clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // 'clean up'
+    // --> cuando el componente se desmonta
+    // --> cuando cambian las dependencias antes de ejecutar el efecto de nuevo
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <h1>PROYECTO 3</h1>
+    <main>
+      <h3>Mouse Follower</h3>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: '0.8',
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Disable' : 'Enable'} pointer follower
+      </button>
+    </main>
   )
 }
 

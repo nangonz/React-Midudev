550c11a49f58edf810dd49ab32ff5ce9f86e992e
nangonz <nangonz91@gmail.com>
2023-02-07 19:26:44 -0300

    Add new service file to get random fact

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,42 @@
 import { useEffect, useState } from 'react'
+import { getRandomFact } from './services/facts'
+import './App.css'
 
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
-  const [fact, setFact] = useState('cats random fact')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(response => response.json())
-      .then(data => setFact(data.fact))
+  useEffect(async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
   }, [])
 
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ', 3).join(' ')
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        console.log(response)
+        setImageUrl(url)
+      })
+  }, [fact])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
+
   return (
-    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <main>
       <h1>App de Gatitos</h1>
+      <button onClick={handleClick}>Get new Fact</button>
       <p>{fact}</p>
+      <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt='Cat says image' />
     </main>
   )
 }
